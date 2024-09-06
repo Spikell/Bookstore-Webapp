44cd3f52b1e6785b9a97ca54632d04a8b0e9744b
Jatin Ochani <jatinochani04@gmail.com>
2024-09-07 03:11:07 +0530

    Configure Toaster for improved Notifications

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,22 @@ import './App.css'
 import { Outlet }  from 'react-router-dom'
 import Navbar  from './components/Navbar'
 import { NextUIProvider } from "@nextui-org/react"
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <NextUIProvider>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        toastOptions={{
+          style: {
+            zIndex: 9999, // Increase this value if needed
+          },
+        }}
+      />
       <Navbar/>
       <Outlet/>
     </NextUIProvider>
