a31d471f023de7b3fa54dd0c9f87c6731ec3b4c7
Jatin Ochani <jatinochani04@gmail.com>
2024-09-13 23:34:53 +0530

    Update App component to include CartDB and Firebase initialization

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,33 @@
-import { useState } from 'react'
+import React from 'react';
+import CartDB from './Firebase/CartDB';
 import './App.css'
 import { Outlet }  from 'react-router-dom'
 import Navbar  from './components/Navbar'
 import { NextUIProvider } from "@nextui-org/react"
 import { Toaster } from 'react-hot-toast';
+import { app } from './Firebase/firebase.config'; // Import the Firebase app
 
 function App() {
-  const [count, setCount] = useState(0)
+  console.log("Firebase app initialized:", app);
 
   return (
-    <NextUIProvider>
-      <Toaster
-        position="top-center"
-        reverseOrder={false}
-        toastOptions={{
-          style: {
-            zIndex: 9999, // Increase this value if needed
-          },
-        }}
-      />
-      <Navbar/>
-      <Outlet/>
-    </NextUIProvider>
-  )
+    <>
+      <CartDB />
+      <NextUIProvider>
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+          toastOptions={{
+            style: {
+              zIndex: 9999,
+            },
+          }}
+        />
+        <Navbar/>
+        <Outlet/>
+      </NextUIProvider>
+    </>
+  );
 }
 
-export default App
+export default App;
