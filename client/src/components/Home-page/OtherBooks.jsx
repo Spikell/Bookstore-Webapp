174b40e491ba0f6614e4f94fdae2a5e6b2ae8886
Jatin Ochani <jatinochani04@gmail.com>
2024-09-17 00:29:04 +0530

    Implement onBookSelect prop to handle book selection

--- client/src/components/Home-page/OtherBooks.jsx
+++ client/src/components/Home-page/OtherBooks.jsx
@@ -1,19 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import BookCards from './BookCards'
 
-const OtherBooks = () => {
+const OtherBooks = ({ onBookSelect }) => {
     const [books, setBooks] = useState([]);
 
     useEffect(() => {
         fetch("http://localhost:5000/all-books")
         .then(res => res.json())
-        .then(data => setBooks(data.slice(8, 16)))
+        .then(data => setBooks(data.slice(8,16)))
     }, []);
-  return (
-    <div>
-      <BookCards books={books} headline="Other Books"/>
-    </div>
-  )
+
+    return (
+        <div>
+            <BookCards books={books} headline="Other Books" onBookSelect={onBookSelect} />
+        </div>
+    )
 }
 
 export default OtherBooks
