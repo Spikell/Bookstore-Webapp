beee29b4a1d39ede1a3ea69c1525decb82bf8931
Jatin Ochani <jatinochani04@gmail.com>
2024-09-17 00:41:14 +0530

    Add state management for selected book

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,22 +1,53 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import Banner from '../components/Home-page/Banner'
 import BestSellingBooks from '../components/Home-page/BestSellingBooks'
 import FavBook from '../components/Home-page/FavBook'
 import PromoBanner from '../components/Home-page/PromoBanner'
 import OtherBooks from '../components/Home-page/OtherBooks'
 import Reviews from '../components/Home-page/Reviews'
+import SingleBook from '../components/SingleBook'
 
 const Home = () => {
+  const [selectedBook, setSelectedBook] = useState(null);
+
+  const handleBookSelect = (book) => {
+    setSelectedBook(book);
+  };
+
+  const handleCloseBook = useCallback(() => {
+    setSelectedBook(null);
+  }, []);
+
+  const addToCart = (book) => {
+    // Implement add to cart functionality
+    console.log('Adding to cart:', book);
+  };
+
+  const handleOverlayClick = useCallback((e) => {
+    if (e.target === e.currentTarget) {
+      handleCloseBook();
+    }
+  }, [handleCloseBook]);
+
   return (
     <div>
-      <Banner/>
-      <BestSellingBooks/>
+      <Banner onBookSelect={handleBookSelect} />
+      <BestSellingBooks onBookSelect={handleBookSelect} />
       <FavBook/>
       <PromoBanner/>
-      <OtherBooks/> 
+      <OtherBooks onBookSelect={handleBookSelect} /> 
       <Reviews/>
+      {selectedBook && (
+        <div 
+          className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+          onClick={handleOverlayClick}
+        >
+          <div className="bg-white rounded-lg w-11/12 max-w-4xl" onClick={e => e.stopPropagation()}>
+            <SingleBook book={selectedBook} onClose={handleCloseBook} addToCart={addToCart} />
+          </div>
+        </div>
+      )}
     </div>
-    
   )
 }
 
