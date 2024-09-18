e60f816705faf23747e141107569d99e43ec164f
Jatin Ochani <jatinochani04@gmail.com>
2024-09-18 22:59:42 +0530

    Add axios config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import CartDB from './Firebase/CartDB';
 import './App.css'
 import { Outlet }  from 'react-router-dom'
 import Navbar  from './components/Navbar'
 import { NextUIProvider } from "@nextui-org/react"
 import { Toaster } from 'react-hot-toast';
-import { app } from './Firebase/firebase.config'; // Import the Firebase app
+import { app } from './Firebase/firebase.config';
 
 function App() {
   console.log("Firebase app initialized:", app);
+
+  useEffect(() => {
+    const makeApiCall = async () => {
+      try {
+        const response = await axios.post("https://bookstore-webapp-api.vercel.app/");
+        console.log("API response:", response.data);
+      } catch (error) {
+        console.error("Error making API call:", error);
+      }
+    };
+
+    makeApiCall();
+  }, []);
 
   return (
     <>
