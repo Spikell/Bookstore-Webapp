164ff7ab69dd36918f4b0e7c4e51e1e63242453f
Jatin Ochani <jatinochani04@gmail.com>
2024-10-08 00:11:21 +0530

    Enhance table design

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -141,12 +141,12 @@ function Cart() {
         <>
           <div className="bg-white shadow-md rounded-lg overflow-hidden">
             <table className="w-full">
-              <thead className="bg-gray-800 text-white">
+              <thead className="bg-gradient-to-r from-teal-400 to-teal-600 text-white">
                 <tr>
-                  <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Product</th>
-                  <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Price</th>
+                  <th className="px-6 py-3 text-center text-xs font-medium uppercase tracking-wider">Product</th>
+                  <th className="px-6 py-3 text-center text-xs font-medium uppercase tracking-wider">Price</th>
                   <th className="px-6 py-3 text-center text-xs font-medium uppercase tracking-wider">Quantity</th>
-                  <th className="px-6 py-3 text-right text-xs font-medium uppercase tracking-wider">Total</th>
+                  <th className="px-6 py-3 text-center text-xs font-medium uppercase tracking-wider">Total</th>
                   <th className="px-6 py-3 text-center text-xs font-medium uppercase tracking-wider">Actions</th>
                 </tr>
               </thead>
