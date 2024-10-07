89c4fc4c83784ce41d79484fa307ebfe342b7ecd
Jatin Ochani <jatinochani04@gmail.com>
2024-10-08 00:25:55 +0530

    Enhance table look

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -143,8 +143,8 @@ function Cart() {
             <table className="w-full">
               <thead className="bg-gradient-to-r from-teal-400 to-teal-600 text-white">
                 <tr>
-                  <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Product</th>
-                  <th className="px-6 py-3 text-center text-xs font-medium uppercase tracking-wider">Price</th>
+                  <th className="pl-10 py-3 text-left text-xs font-medium uppercase tracking-wider">Product</th>
+                  <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Price</th>
                   <th className="px-6 py-3 text-center text-xs font-medium uppercase tracking-wider">Quantity</th>
                   <th className="px-6 py-3 text-right text-xs font-medium uppercase tracking-wider">Total</th>
                   <th className="px-6 py-3 text-center text-xs font-medium uppercase tracking-wider">Actions</th>
